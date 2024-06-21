9c31a29f516656b0b7db18b2c09263f37d238db5
borvux <bennyjoram@email.com>
2024-06-21 15:07:24 -0500

    fixed form of index

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,10 +1,18 @@
 class DeliveriesController < ApplicationController
   def index
-    matching_deliveries = Delivery.all
 
-    @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+    # if there is no user in the cookie, then redirects to the login page
+    if current_user == nil
+      redirect_to("/users/sign_in")
 
-    render({ :template => "deliveries/index" })
+    # else if there is user in the cookie then render the index page of deliveries 
+    else
+      matching_deliveries = Delivery.all
+
+      @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+
+      render({ :template => "deliveries/index" })
+    end
   end
 
   def show
@@ -19,11 +27,11 @@ class DeliveriesController < ApplicationController
 
   def create
     the_delivery = Delivery.new
-    the_delivery.user_id = params.fetch("query_user_id")
+    the_delivery.user_id = current_user.id
     the_delivery.description = params.fetch("query_description")
     the_delivery.details = params.fetch("query_details")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = false
 
     if the_delivery.valid?
       the_delivery.save
